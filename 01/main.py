fe0bb684ebf808dcae5ee3c5d8d714a97d15e17f
marcisarajs <marcis.arajs@grobinasskola.lv>
2024-12-04 14:28:38 +0200

    Pierakstīju visus komentārus

--- 01/main.py
+++ 01/main.py
@@ -1,11 +1,15 @@
 """ Insert Name """
+# Izveidoja mainīgo name, kurā ievada savu vārdu
 name = input("Whats your name: ")
 
 """ Insert age. Also convert age into an Integer value """
+# Izveidoja mainīgo age, kurā ievada savu vecumu. Mainīgo arī pārvērta par int vērtību.
 age = int(input("How old are you: "))
 
 """ Do the math """
+# Izveidoja mainīgo year, kura vērtība ir tagadējais gads mīnus age mainīgais plus 100
 year = str((2018 - age)+100)
 
 """ Print the result """
+# Izprintē name mainīgo ar tekstu, kas saka cik vecs tu būsi pēc 100 gadiem, un tad vēl pieliek klāt year mainīgo
 print(name + " will be 100 years old in the year " + year)
